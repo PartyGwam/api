fdad05f84017304136e1e054fb045bef0c65bd10
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-06-29 10:08:59 +0900

    Issues #12, #13 - 프로필 조회 / 수정 의 스켈레톤

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -3,6 +3,23 @@ from django.conf import settings
 from django.db import models
 
 
+class ProfileManager(models.Manager):
+    def create_profile(self, user, username, **kwargs):
+        if not user:
+            raise ValueError('프로필은 user 가 있는 상태에서만 생성 가능합니다.')
+        if not username:
+            raise ValueError('닉네임은 필수입니다.')
+
+        profile = Profile(
+            user=user,
+            username=username,
+            **kwargs
+        )
+
+        profile.save(using=self._db)
+        return profile
+
+
 class Profile(models.Model):
     uuid = models.UUIDField(
         primary_key=True,
@@ -11,11 +28,16 @@ class Profile(models.Model):
         default=uuid.uuid4,
         verbose_name='프로필 pk'
     )
-    profile_of_user = models.OneToOneField(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
         editable=False,
         verbose_name='해당 프로필의 유저'
+    )
+    username = models.CharField(
+        max_length=8,
+        unique=True,
+        verbose_name='닉네임'
     )
     profile_picture = models.FileField(
         upload_to='assets/images/',
@@ -28,7 +50,12 @@ class Profile(models.Model):
     )
     is_active = models.BooleanField(default=True, verbose_name='활성화 여부')
 
+    objects = ProfileManager()
+
     class Meta:
         db_table = 'profiles'
         verbose_name = '프로필'
         verbose_name_plural = '프로필들'
+
+    def __str__(self):
+        return '{} 의 프로필'.format(self.user)
