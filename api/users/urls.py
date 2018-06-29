202970eaade6bf69d0b7a545856c9995561e5470
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-06-29 18:49:18 +0900

    Hotfix: 이메일 및 닉네임 실시간 검증을 위한 API 추가

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from api.users.views import LoginAPIView, UserAPIView
+from api.users.views import \
+    LoginAPIView, UserAPIView, EmailValidateAPIView, UsernameValidateAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view()),
-    path('', UserAPIView.as_view())
+    path('', UserAPIView.as_view()),
+    path('validate/email/', EmailValidateAPIView.as_view()),
+    path('validate/username/', UsernameValidateAPIView.as_view()),
 ]
