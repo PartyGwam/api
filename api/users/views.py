b769a7e43d7322a317b28989c08b6b1826768f29
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-07-03 19:28:22 +0900

    중복 코드 제거

--- api/users/views.py
+++ api/users/views.py
@@ -100,27 +100,6 @@ class UserAPIView(generics.ListCreateAPIView):
             status=status.HTTP_201_CREATED
         )
 
-class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return UserSerializer
-        else:
-            return UserPasswordSerializer
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance=instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        User.objects.deactivate(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
