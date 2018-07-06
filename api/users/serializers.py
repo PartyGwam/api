dff2c7739e59db3332d5ec37e134af7524b7bc40
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-07-06 18:00:59 +0900

    Drop unnecessary serializer depth

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -45,7 +45,6 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
-        depth = 2
 
     def create(self, validated_data):
         model_class = self.Meta.model
