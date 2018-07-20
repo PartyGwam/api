6e782e63d758872831c546558a87448a24c22e62
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-07-20 20:42:03 +0900

    Fix URL conf

--- api/notifications/urls.py
+++ api/notifications/urls.py
@@ -3,10 +3,6 @@ from rest_framework.routers import SimpleRouter
 from api.notifications.views import NotificationAPIViewSet
 
 router = SimpleRouter()
-router.register(
-    'notifications',
-    NotificationAPIViewSet,
-    base_name='notification'
-)
+router.register('', NotificationAPIViewSet, base_name='notification')
 
 urlpatterns = router.urls
